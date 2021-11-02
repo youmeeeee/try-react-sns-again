c2a56cb2a78544cdde20d18f9aaecf94c6237965
youmekko <youmekko.o@gmail.com>
2021-11-02 22:54:25 +0900

    day7_이미지 캐러셀 구현

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -4,12 +4,29 @@ import Link from 'next/link'
 import { Menu, Input, Row, Col } from 'antd'
 import styled from 'styled-components'
 import { useSelector} from 'react-redux'
+import { createGlobalStyle } from 'styled-components'
 
 import LoginForm from '../components/LoginForm'
 import UserProfile from '../components/UserProfile'
 
+
 const SearchInput = styled(Input.Search)`
     vertical-align: middle;
+`
+
+const Global = createGlobalStyle`
+    .ant-row {
+        margin-right: 0 !important;
+        margin-left: 0 !important;
+    }
+
+    .ant-col:first-child {
+        padding-left: 0 !important;
+    }
+
+    .ant-col:last-child {
+        padding-right: 0 !important;
+    }
 `
 
 const AppLayout = ({children}) => {
@@ -17,6 +34,7 @@ const AppLayout = ({children}) => {
     
     return (
         <div>
+            <Global />
             <Menu mode="horizontal">
                 <Menu.Item key="home">
                     <Link href="/"><a>SNS</a></Link>
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,6 +1,6 @@
 import { createWrapper } from 'next-redux-wrapper'
 import { applyMiddleware, compose, createStore } from 'redux'
-import {composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducer from '../reducers'
 
