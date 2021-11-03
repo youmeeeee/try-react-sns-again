229f4da36a135191f85ef28da79e0963a29e4c0c
youmekko <youmekko.o@gmail.com>
2021-11-03 22:59:59 +0900

    day8_redux-saga middleware 추가하고 rootSaga 구현 (redux-thunk제거)

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,24 +1,24 @@
 import { createWrapper } from 'next-redux-wrapper'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import thunkMiddleware from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
 import reducer from '../reducers'
+import rootSaga from '../sagas'
 
-const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+const loggerMiddleware = ({ dispatch, getState }) => (next) => action => {
     console.log(action)
-    // if (typeof action === 'function') {
-    //     return action(dispatch, getState)
-    // }
     return next(action)
 }
 
 const configureStore = () => {
-    const middlewares = [loggerMiddleware, thunkMiddleware] 
+    const sagaMiddleware = createSagaMiddleware()
+    const middlewares = [sagaMiddleware, loggerMiddleware] 
     const enhancer = process.env.NODE_ENV === 'production' 
         ? compose(applyMiddleware(...middlewares))
         : composeWithDevTools(applyMiddleware(...middlewares))
     const store = createStore(reducer, enhancer)
+    store.sagaTask = sagaMiddleware.run(rootSaga)
     return store
 }
 
