18bdc5374197ffb3011eb570b729b21d618430bc
Yumi Choi <youmechoi1129@gmail.com>
2021-11-05 16:00:10 +0900

    day9_오타수정

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -23,7 +23,7 @@ const configureStore = () => {
 }
 
 const wrapper = createWrapper(configureStore, {
-    debug: process.env.NODE_ENV === 'dvelopement'
+    debug: process.env.NODE_ENV === 'development'
 })
 
 export default wrapper
