ab74d33daab46b086f89d152d4780f5b08fe7820
youmekko <youmekko.o@gmail.com>
2021-12-25 23:20:36 +0900

    day37_오타수정

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -33,7 +33,6 @@ export default class MyDocument extends Document {
         <Head />
         <body>
           <Main />
-          <script src="https://polyfill.io/v3/polyfill.min.js?features=blissfuljs%2Cdefault%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces2020" />
           <NextScript />
         </body>
       </Html>
