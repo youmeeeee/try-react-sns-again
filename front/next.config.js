26f8dcd49c0085c15d40909619885558404c43ad
youmekko <youmekko.o@gmail.com>
2022-02-25 10:07:10 +0900

    nuxt/bundle-analyzer 추가

---
+++ front/next.config.js
@@ -0,0 +1,20 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enable: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production'
+    const plugins = [
+      ...config.plugins,
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko^/),
+    ]
+    return {
+      ...config,
+      mode: prod ? 'production' : 'devlopment',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins,
+    }
+  },
+})
